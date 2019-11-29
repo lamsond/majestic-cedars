f571881afecc19c6ca27481240979229f67a4f24
lamsond <dlamson13@gmail.com>
2019-11-29 18:03:32 -0500

    create animation thread

--- src/org/whrhs/dlamson/hockey/Puck.java
+++ src/org/whrhs/dlamson/hockey/Puck.java
@@ -12,12 +12,17 @@ public class Puck {
 	public Puck(int xOffset, int yOffset) {
 		x = Ice.LENGTH/2 + xOffset;
 		y = Ice.WIDTH/2 + yOffset;
-		speed = 3;
+		speed = 5;
 		vector = new double[]{1.0, 0.0};
 	}
 	
 	public void draw(Graphics2D g, Color c) {
 		g.setColor(c);
 		g.fillArc(x - PUCK_DIAMETER/2,  y - PUCK_DIAMETER/2,  PUCK_DIAMETER,  PUCK_DIAMETER, 0, 360);
+	}
+	
+	public void move() {
+		x += (int)(vector[0] * speed);
+		y += (int)(vector[1] * speed);
 	}
 }
