663f1fdcc3213428a4a70e70b91cf1e5dec514f5
lamsond <dlamson13@gmail.com>
2020-01-16 14:47:28 -0500

    review for arr test

---
+++ src/org/whrhs/dlamson/csa/u6/ex/ArrayTacos.java
@@ -0,0 +1,5 @@
+package org.whrhs.dlamson.csa.u6.ex;
+
+public class ArrayTacos {
+
+}
---
+++ src/org/whrhs/dlamson/csa/u6/ex/Song.java
@@ -0,0 +1,16 @@
+package org.whrhs.dlamson.csa.u6.ex;
+
+public class Song {
+	
+	private String title;
+	private String artistName;
+	private int seconds;
+	
+	public Song(String title, String artistName, int seconds) {
+		this.title = title;
+		this.artistName = artistName;
+		this.seconds = seconds;
+	}
+	
+	// getters and setters not shown
+}
