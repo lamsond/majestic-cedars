5d961758ad796ac56fee9c3a6d4a780203cebd93
lamsond <dlamson13@gmail.com>
2020-01-27 15:17:03 -0500

    test checks tbc

--- src/org/whrhs/dlamson/csa/u7/exam/TestChecker.java
+++ src/org/whrhs/dlamson/csa/u7/exam/TestChecker.java
@@ -2,4 +2,11 @@ package org.whrhs.dlamson.csa.u7.exam;
 
 public class TestChecker {
 
+	public static void main(String[] args) {
+		
+		int[] nums = {1, 34, 54, 2, 3};
+		
+		// can't clone directly to double	
+		
+	}
 }
