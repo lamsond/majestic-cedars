b3ae8436f0b5bde5a315f9039f267bb80ecee49f
lamsond <dlamson13@gmail.com>
2020-03-24 19:10:27 -0400

    simulation mods

--- src/org/whrhs/dlamson/csa/u9/l4/HandTester.java
+++ src/org/whrhs/dlamson/csa/u9/l4/HandTester.java
@@ -6,8 +6,32 @@ public class HandTester {
 	
 	public static void main(String[] args) {
 		
-		BlackjackHand hand = new BlackjackHand();
-		System.out.println(hand);
+		HashMap<Integer, Double> results = new HashMap<>();
+		int trials = 1_000_000_000;
+		int ties = 0;
+		
+		for(int i = 0; i < trials; i++) {
+			
+			BlackjackHand hand1 = new BlackjackHand();
+			BlackjackHand hand2 = new BlackjackHand();
+			if(hand1.equals(hand2)) {
+				ties++;
+			}
+			
+			if(results.containsKey(hand1.getValue())) {
+				results.put(hand1.getValue(), results.get(hand1.getValue()) + 1);
+			}
+			else {
+				results.put(hand1.getValue(), 1.0);
+			}
+		}
+		
+		for(int i = 4; i <= 21; i++) {
+			results.put(i, results.get(i)/trials*100);
+		}
+		System.out.println(results);
+		System.out.println((double)ties/trials*100);
+		
 	}
 
 }
