0a4082470631ba5bff99c45d6a3c22d3c6741192
lamsond <dlamson13@gmail.com>
2020-11-24 13:44:14 -0500

    working on U5L1

---
+++ src/org/whrhs/dlamson/csa20/u5/l1/Paddle.java
@@ -0,0 +1,14 @@
+package org.whrhs.dlamson.csa20.u5.l1;
+
+import java.awt.Color;
+
+public class Paddle {
+
+	public static final int WIDTH = 15;
+	public static final int HEIGHT = 50;
+
+	private int x, y, speed;
+	private boolean computerControlled;
+	private Color color;
+	
+}
